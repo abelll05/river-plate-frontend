07ea59b58438ba1c3ef65b24e1355c7ca14101f5
abelll05 <abelmerc45@gmail.com>
2024-12-05 02:57:48 -0300

    Actualizar configuración para el deploy

--- src/components/Register.js
+++ src/components/Register.js
@@ -20,7 +20,7 @@ const Register = () => {
     setError('');
 
     try {
-      const response = await fetch('http://localhost:5000/api/register', {
+      const response = await fetch('https://river-plate-frontend.onrender.com/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
